d1a25e8d9f348056a27cca0ab08502e0eace5dff
nettoreyes <nettoreyes@gmail.com>
2021-05-06 14:58:09 -0400

    segunda modificacion de prueba en archivo app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo Coder
+          Hola Mundo Coder desde ReactJS
         </p>
         <a
           className="App-link"
