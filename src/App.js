b6794842fcfa023352530c6533690edadd62bcd6
nettoreyes <nettoreyes@gmail.com>
2021-05-10 09:48:13 -0400

    entrega desafio Menú e-commerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo Coder desde ReactJS
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+        {/* <h1>Hola Mundo Coder desde ReactJS</h1> */}
+        <NavBar />
     </div>
   );
 }
