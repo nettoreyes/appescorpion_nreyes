74312ded0a06d70586350e3ddae4b389252049a8
nettoreyes <nettoreyes@gmail.com>
2021-06-17 19:30:41 -0400

    se modifica visualizacion de  cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,79 +1,73 @@
 import React, { useContext } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Image from "react-bootstrap/Image";
 import { CartContext } from "../context/CartContext";
 
 const Cart = () => {
   const { listaCarro, eliminaItem } = useContext(CartContext);
-  let linea = 0;
-  let totalFinal=0;
+  
+  let totalFinal = 0;
 
-  for(const item of listaCarro){
-     item.totalLinea = item.cantidad * item.price;
-     totalFinal += item.totalLinea;
+  for (const item of listaCarro) {
+    item.totalLinea = item.cantidad * item.price;
+    totalFinal += item.totalLinea;
   }
 
   if (listaCarro.length > 0) {
     return (
       <div className="container mt-5">
         <h1>Lista de compra</h1>
+        <div className="container">
+        {
+        listaCarro.map((item) => (
+          <div className="row pb-2 my-1 align-items-center border border-success rounded-3 g-2" key={item.idDetalle}>            
+            <div className="col-12 col-md-2 text-center">
+              <Image variant="top" src={`${item.image}/100`} />
+            </div>
+            <div className="col-12 col-md-8 ">
+              <div className="row p-1 p-md-3 align-items-center g-1">
+                <div className="col-12 text-start fw-bold">{item.title}</div>                
+                <div className="w-100 d-none d-md-block"></div>
+                <div className="col-3 text-start"> $ { item.price.toLocaleString('de-DE') } c/u</div>
+                <div className="col-6 text-center">
+                  <button className="btn btn-success btn-sm" onClick={() => eliminaItem(item.idProducto)}> - </button>
+                  <span className="mx-md-2"> {item.cantidad} </span>
+                  <button className="btn btn-success btn-sm" onClick={() => eliminaItem(item.idProducto)}> + </button>
+                </div>
+                <div className="col-3 text-end fw-bold"> ${item.totalLinea.toLocaleString('de-DE')} </div>
 
-        <table className="table table-striped table-hover align-middle">
-          <thead className="table-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col"></th>
-              <th scope="col">Detalle</th>
-              <th scope="col" className="text-end">
-                Precio
-              </th>
-              <th scope="col" className="text-end">
-                Cantidad
-              </th>
-              <th scope="col" className="text-end">
-                Total
-              </th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              listaCarro.map((item) => (
-                <tr key={item.idDetalle}>
-                  <th scope="row">{(linea += 1)} </th>
-                  {/* <td><Image variant="top" src={`https://picsum.photos/id/${item.pictureUrl}/100`} /></td> */}
-                  <td><Image variant="top" src={`${item.image}/100`}  /></td>                  
-                  <td>{item.title}</td>
-                  <td className="text-end">{item.price}</td>
-                  <td className="text-end">{item.cantidad}</td>
-                  <td className="text-end">{item.totalLinea}</td>
-                  <td className="text-center"><button className="btn btn-danger btn-sm px-2" onClick={() => eliminaItem(item.idProducto)} > X </button></td>
-                </tr>
-              ))
-            }
-          </tbody>
-          <tfoot>
-            <tr>
-              <th scope="col"></th>
-              <th scope="col"></th>
-              <th scope="col"></th>
-              <th scope="col" className="text-end"></th>
-              <th scope="col" className="text-end">Total</th>
-              <th scope="col" className="text-end">${totalFinal}</th>
-              <th className="text-center"><button className="btn btn-danger btn-sm px-2" onClick={() => eliminaItem("0")} ><i className="bi bi-trash"></i> Vaciar Carro</button></th>
-            </tr>
-          </tfoot>
-        </table>
-        <Link to={`/`} className="btn btn-primary btn-sm">Seguir comprando</Link>
+              </div>
+            </div>
+            <div className="col-12 col-md-2 text-sm-start text-md-center align-self-end"><button className="btn btn-danger btn-sm" onClick={() => eliminaItem(item.idProducto)}>Eliminar</button></div>
+          </div>
+        ))
+        }
+        </div>
+        <div className="container">
+          <div className="row g2 p-1 align-items-center justify-content-end rounded-3">
+            <div className="col-12 col-md-3 text-center text-md-start">
+              <Link to={`/`} className="btn btn-primary btn-sm">Seguir comprando</Link>
+            </div>
+            <div className="col-12 col-md-3 mt-1 text-center text-md-end">
+              <h3>Total : ${ totalFinal.toLocaleString('de-DE') }</h3>
+            </div>
+            <div className="col-12 col-md-2 mt-1 text-center">
+              <Link to={`/`} className="btn btn-success btn-sm">Terminar Compra</Link>
+            </div>
+          </div>
+        </div>
+
         
-      </div>
+        </div>
     );
   } else {
     return (
-    <div className="container m-5">
-      <h1>Carro Vacio</h1>
-      <Link to={`/`} className="btn btn-primary btn-sm">Volver</Link>
-    </div>
+      <div className="container m-5">
+        <h1>Carro Vacio</h1>
+        <Link to={`/`} className="btn btn-primary btn-sm">
+          Volver
+        </Link>
+      </div>
     );
   }
 };
